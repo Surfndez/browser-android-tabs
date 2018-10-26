8a4d9b97560707d50936d93601b030f746293879
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2018-10-26 16:38:17 -0400

    opening rewards-panel on rewards-button click

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -48,8 +48,10 @@ import androidx.annotation.StringRes;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.TraceEvent;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.compositor.Invalidator;
 import org.chromium.chrome.browser.compositor.layouts.LayoutUpdateHost;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
@@ -143,6 +145,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     protected View mUrlActionContainer;
     protected ImageView mToolbarShadow;
     private @Nullable ImageButton mExperimentalButton;
+    private ImageView mBraveRewardsPanelButton;
 
     private ObjectAnimator mTabSwitcherModeAnimation;
     private ObjectAnimator mDelayedTabSwitcherModeAnimation;
@@ -453,6 +456,10 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         if (mBraveShieldsButton != null) {
             mBraveShieldsButton.setClickable(true);
         }
+        mBraveRewardsPanelButton = (ImageView) findViewById(R.id.brave_rewards_button);
+        if (mBraveRewardsPanelButton != null) {
+            mBraveRewardsPanelButton.setClickable(true);
+        }
     }
 
     private void enableTabSwitchingResources() {
@@ -474,6 +481,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         });
         mBraveShieldsButton.setOnClickListener(this);
         mBraveShieldsButton.setOnLongClickListener(this);
+        mBraveRewardsPanelButton.setOnClickListener(this);
     }
 
     /**
@@ -559,6 +567,10 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         } else if (mBraveShieldsButton == v) {
             if (null != mBraveShieldsButton) {
                 mBraveShieldsListener.onClick(mBraveShieldsButton);
+            }
+        } else if (mBraveRewardsPanelButton == v) {
+            if (null != mBraveRewardsPanelButton) {
+                PopupActivity.show((ChromeActivity) ApplicationStatus.getLastTrackedFocusedActivity());
             }
         }
     }
