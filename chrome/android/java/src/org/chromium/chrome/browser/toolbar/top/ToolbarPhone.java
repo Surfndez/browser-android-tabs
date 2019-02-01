dfa1337b879cabceaa3cac09c572535d03ed7a57
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-02-01 15:59:00 -0500

    message about not signed contributors

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2091,6 +2091,9 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     public void OnIsWalletCreated(boolean created) {}
 
     @Override
+    public void OnGetPendingContributionsTotal(double amount) {}
+
+    @Override
     public void setOnTabSwitcherClickHandler(OnClickListener listener) {
         if (mToggleTabStackButton != null) {
             mToggleTabStackButton.setOnTabSwitcherClickHandler(listener);
