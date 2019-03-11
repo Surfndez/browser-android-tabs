fbe969f36a6a0432f14c7fe3d4ad5538efc6de12
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-03-11 14:43:52 -0400

    Thank you page (publisher banner) #1131. Part 2

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2155,6 +2155,9 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     @Override
     public void OnGetAutoContributeProps() {}
 
+    @Override
+    public void OnGetReconcileStamp(long timestamp){}
+
 
     @Override
     public void setOnTabSwitcherClickHandler(OnClickListener listener) {
