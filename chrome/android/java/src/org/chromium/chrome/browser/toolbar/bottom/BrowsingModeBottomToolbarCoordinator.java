8d98e970e59c1549d01ac19c5f0a4a5a440b801c
Albert Wang <amwang217@gmail.com>
2019-03-13 00:58:28 -0700

    Enable SearchAccelerator at the bottom toolbar

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarCoordinator.java
@@ -26,12 +26,12 @@ import org.chromium.chrome.browser.toolbar.BookmarksButton;
 import org.chromium.chrome.browser.toolbar.HomeButton;
 import org.chromium.chrome.browser.toolbar.IncognitoStateProvider;
 import org.chromium.chrome.browser.toolbar.MenuButton;
-import org.chromium.chrome.browser.toolbar.NewTabButton;
 import org.chromium.chrome.browser.toolbar.TabCountProvider;
 import org.chromium.chrome.browser.toolbar.TabSwitcherButtonCoordinator;
 import org.chromium.chrome.browser.toolbar.TabSwitcherButtonView;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuButtonHelper;
 import org.chromium.components.feature_engagement.FeatureConstants;
+import org.chromium.chrome.browser.toolbar.bottom.SearchAccelerator;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
@@ -126,6 +126,8 @@ public class BrowsingModeBottomToolbarCoordinator {
             }
         });
 
+        mSearchAccelerator = toolbarRoot.findViewById(R.id.search_accelerator);
+        mSearchAccelerator.setOnClickListener(searchAcceleratorListener);
 
         mTabSwitcherButtonCoordinator = new TabSwitcherButtonCoordinator(mToolbarRoot);
         // TODO(amaralp): Make this adhere to MVC framework.
@@ -221,10 +223,6 @@ public class BrowsingModeBottomToolbarCoordinator {
             mHomeButton.setOverviewModeBehavior(overviewModeBehavior);
         }
 
-        mNewTabButton = mToolbarRoot.findViewById(R.id.new_tab_button);
-        mNewTabButton.setThemeColorProvider(themeColorProvider);
-        mNewTabButton.setOnClickListener(newTabClickListener);
-
         mBookmarksButton.setThemeColorProvider(themeColorProvider);
     }
 
@@ -307,10 +305,9 @@ public class BrowsingModeBottomToolbarCoordinator {
         mMediator.destroy();
         mHomeButton.destroy();
         //mShareButton.destroy();
-        //mSearchAccelerator.destroy();
+        mSearchAccelerator.destroy();
         mTabSwitcherButtonCoordinator.destroy();
         mMenuButton.destroy();
-        mNewTabButton.destroy();
         mBookmarksButton.destroy();
     }
 }
