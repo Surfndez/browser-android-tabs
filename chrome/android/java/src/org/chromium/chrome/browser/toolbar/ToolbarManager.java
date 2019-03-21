633418674beed55a3acbef136a7761d891740bff
samartnik <artem@brave.com>
2019-03-21 15:18:13 -0400

    Changed bottom toolbar bookmarks button

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1100,6 +1100,7 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
                     mActivity.getCompositorViewHolder().getLayoutManager(),
                     wrapBottomToolbarClickListenerForIPH(tabSwitcherClickHandler),
                     wrapBottomToolbarClickListenerForIPH(newTabClickHandler),
+                    wrapBottomToolbarClickListenerForIPH(bookmarkClickHandler),
                     closeTabsClickListener, mAppMenuButtonHelper, mOverviewModeBehavior,
                     mActivity.getWindowAndroid(), mTabCountProvider, mIncognitoStateProvider,
                     mActivity.findViewById(R.id.control_container));
@@ -1883,6 +1884,9 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
         boolean editingAllowed = currentTab == null || mBookmarkBridge == null
                 || mBookmarkBridge.isEditBookmarksEnabled();
         mToolbar.updateBookmarkButton(isBookmarked, editingAllowed);
+        if (mBottomToolbarCoordinator != null) {
+            mBottomToolbarCoordinator.updateBookmarkButton(isBookmarked, editingAllowed);
+        }
     }
 
     private void updateReloadState(boolean tabCrashed) {
