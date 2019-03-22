d8fdc807ddc7771f064e994d96fe9d7aded7d830
samartnik <artem@brave.com>
2019-03-22 11:27:19 -0400

    Added disable bootom toolbar menu item. Changed icons.

--- chrome/android/java/src/org/chromium/chrome/browser/flags/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/flags/FeatureUtilities.java
@@ -117,7 +117,7 @@ public class FeatureUtilities {
     public static void cacheNativeFlags() {
         cacheCommandLineOnNonRootedEnabled();
         FirstRunUtils.cacheFirstRunPrefs();
-        cacheBottomToolbarEnabled();
+        // cacheBottomToolbarEnabled();
         cacheAdaptiveToolbarEnabled();
         cacheLabeledBottomToolbarEnabled();
         cacheNightModeAvailable();
