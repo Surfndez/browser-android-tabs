7506127655e0bd38dc71f3484839ed42f092871a
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2020-01-07 09:59:25 -0500

    fix verified icon flashes. Issue# 2328 (#2335)
    fix verified icon flashes. Issue# 2328

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -395,6 +395,8 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
                 ToolbarColors.getThemedToolbarIconTint(getContext(), true).getDefaultColor();
         mDarkModeDefaultColor =
                 ToolbarColors.getThemedToolbarIconTint(getContext(), false).getDefaultColor();
+        SharedPreferences sharedPref = ContextUtils.getAppSharedPreferences();
+        isInitialNotificationPosted = !sharedPref.getBoolean(BraveRewardsPanelPopup.PREF_WAS_TOOLBAR_BAT_LOGO_BUTTON_PRESSED, false);
     }
 
     @Override
@@ -633,9 +635,8 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
             if (null == mRewardsPopup) {
                 mRewardsPopup = new BraveRewardsPanelPopup(v);
                 mRewardsPopup.showLikePopDownMenu();
-                if (mBraveRewardsNotificationsCount.isShown()) {
-                    SharedPreferences sharedPref = ContextUtils.getAppSharedPreferences();
-                    SharedPreferences.Editor editor = sharedPref.edit();
+                if (isInitialNotificationPosted){
+                    SharedPreferences.Editor editor = ContextUtils.getAppSharedPreferences().edit();
                     editor.putBoolean(BraveRewardsPanelPopup.PREF_WAS_TOOLBAR_BAT_LOGO_BUTTON_PRESSED, true);
                     editor.apply();
                     mBraveRewardsNotificationsCount.setVisibility(View.INVISIBLE);
