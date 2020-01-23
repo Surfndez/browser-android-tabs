remzert/BraveBrowser

Added HTTPSE support
Auditors: @diracdeltas

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -351,6 +351,12 @@ static void SetSafeBrowsingEnabled(JNIEnv* env,
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void SetHTTPSEEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kHTTPSEEnabled, enabled);
+}
+
 static void SetAdBlockEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {

Surfndez/browser-android-tabs

Conflicts:
	chrome/browser/net/chrome_network_delegate.cc
	chrome/android/java/strings/android_chrome_strings.grd

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -85,6 +85,12 @@ static jboolean JNI_PrefServiceBridge_IsManagedPreference(
       PrefServiceBridge::GetPrefNameExposedToJava(j_pref_index));
 }
 
+static void JNI_PrefServiceBridge_SetHTTPSEEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kHTTPSEEnabled, enabled);
+}
+
 static void JNI_PrefServiceBridge_SetAdBlockEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {